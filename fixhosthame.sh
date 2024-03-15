3aa2276c44a8960c2e36738f3d0d9000a4f0e1f1
sujitp81 <sujitp81@hotmail.com>
2024-03-16 04:09:13 +0530

    ss

--- fixhosthame.sh
+++ fixhosthame.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-git pull
-/usr/bin/python3 /usr/local/bin/TKM_R98_LAB_Scripts/FixHostName.py
+git pull || true
+touch /home/labadmin/stuff.stuff
+/usr/bin/python3 /usr/local/bin/TKM_R98_LAB_Scripts/FixHostName.py > /home/labadmin/fixhostname.txt
+
