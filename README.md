0e82a40aa3c9d3190754422444942898f9de921e
sujitp81 <sujitp81@hotmail.com>
2024-03-16 04:17:58 +0530

    stugfff

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TKM_R98_LAB_Scripts
 /usr/lib/systemd/system/fixhostname.service
+nano /usr/lib/systemd/system/fixhostname.service
+sudo systemctl status fixhostname.service
--- fixhosthame.sh
+++ fixhosthame.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-git pull || true
+pwd
+git -C /usr/local/bin/TKM_R98_LAB_Scripts pull || true
 touch /home/labadmin/stuff.stuff
 /usr/bin/python3 /usr/local/bin/TKM_R98_LAB_Scripts/FixHostName.py > /home/labadmin/fixhostname.txt
 
