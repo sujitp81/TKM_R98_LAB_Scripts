72bbf10572127116498182df6820863874533e89
sujitp81 <sujitp81@hotmail.com>
2024-03-17 00:42:13 +0530

    newrelic config file update

--- fixhosthame.sh
+++ fixhosthame.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pwd
 git -C /usr/local/bin/TKM_R98_LAB_Scripts pull || true
-touch /home/labadmin/stuff.stuff
+cp -fr /usr/local/bin/TKM_R98_LAB_Scripts/newrelic-infra.yml /etc/newrelic-infra.yml || true
 /usr/bin/python3 /usr/local/bin/TKM_R98_LAB_Scripts/FixHostName.py
 
